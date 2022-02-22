bac972ce6e671afdc3b7419186a3d09fee972c42
thesurajkmr <surajkmr32@gmail.com>
2022-02-22 09:33:45 +0530

    tut 19

--- React/hello-world/src/components/Namelist.js
+++ React/hello-world/src/components/Namelist.js
@@ -2,34 +2,9 @@ import React from "react";
 import Person from "./Person";
 
 function Namelist() {
-  const persons = [
-    {
-      id: 1,
-      name: "Diana",
-      age: 20,
-      skill: "Angular",
-    },
-    {
-      id: 2,
-      name: "Clark",
-      age: 21,
-      skill: "Vue",
-    },
-    {
-      id: 3,
-      name: "Bruce",
-      age: 22,
-      skill: "React",
-    },
-    {
-      id: 4,
-      name: "Aqua",
-      age: 23,
-      skill: "Node",
-    },
-  ];
-  const personList = persons.map((person) => <Person key={person.id} person={person} />);
-  return <div>{personList}</div>;
+    const persons=['Bruce','Clark','Diana','Bruce'];
+    const personsList=persons.map((person,index)=><h2 key={index}> {index} {person}</h2>);
+    return personsList;
 }
 
 export default Namelist;
