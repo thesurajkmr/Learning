e4daa398d03fbead27c4e6d374182737395171bc
thesurajkmr <surajkmr32@gmail.com>
2022-02-26 16:42:20 +0530

    hook 13

--- react-hooks/src/Components/DataFetching.js
+++ react-hooks/src/Components/DataFetching.js
@@ -1,22 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 function DataFetching() {
-    const [posts,setposts]=useState([])
+    const [post,setpost]=useState({})
+    const [id,setid]=useState(1)
     useEffect(()=>{
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
         .then(response=>{
             console.log(response)
-            setposts(response.data)
+            setpost(response.data)
         })
         .catch(e=>console.log(e))
-    })
+    },[id])
   return (
     <div>
-        <ul>
+        <input type="text" value={id} onChange={(e)=>setid(e.target.value)}></input>
+        <h1>{post.title}</h1>
+        {/* <ul>
             {
                 posts.map(post=><li key={post.id}>{post.title}</li>)
             }
-        </ul>
+        </ul> */}
     </div>
   )
 }
