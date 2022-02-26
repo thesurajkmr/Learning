7c4f9bbe5cb051ea69cac9355266be3babce1c51
thesurajkmr <surajkmr32@gmail.com>
2022-02-26 16:52:34 +0530

    hook 14 added

--- react-hooks/src/Components/DataFetching.js
+++ react-hooks/src/Components/DataFetching.js
@@ -3,6 +3,15 @@ import axios from 'axios'
 function DataFetching() {
     const [post,setpost]=useState({})
     const [id,setid]=useState(1)
+    const [idFromButtonClick,setidFromButtonClick]=useState(1)
+    const clickHandler=()=>{
+        if(id.length==0)
+        {
+            alert("Enter Something")
+            return ;
+        }
+        setidFromButtonClick(id)
+    }
     useEffect(()=>{
         axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
         .then(response=>{
@@ -10,10 +19,11 @@ function DataFetching() {
             setpost(response.data)
         })
         .catch(e=>console.log(e))
-    },[id])
+    },[idFromButtonClick])
   return (
     <div>
         <input type="text" value={id} onChange={(e)=>setid(e.target.value)}></input>
+        <button onClick={clickHandler}>Fetch Post</button>
         <h1>{post.title}</h1>
         {/* <ul>
             {
