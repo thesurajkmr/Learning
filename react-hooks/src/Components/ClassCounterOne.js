0ea3b4800fd707f2b5f715aa64bc74d08934dc0d
thesurajkmr <surajkmr32@gmail.com>
2022-02-26 11:23:59 +0530

    hook8

--- react-hooks/src/Components/ClassCounterOne.js
+++ react-hooks/src/Components/ClassCounterOne.js
@@ -1,35 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class ClassCounterOne extends Component {
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         count:0
-      }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+      name: "",
+    };
+  }
+  incrementCounter = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+  componentDidMount() {
+    document.title = `you have clicked ${this.state.count} times`;
+    return true;
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.count != this.state.count) {
+      console.log("Updated");
+      document.title = `you have clicked ${this.state.count} times`;
     }
-    incrementCounter=()=>{
-        this.setState({
-            count:this.state.count+1
-        })
-    }
-    componentDidMount(){
-        document.title=`you have clicked ${this.state.count} times`;
-        return true;
-    }
-    componentDidUpdate(){
-        document.title=`you have clicked ${this.state.count} times`;
-    }
+  }
 
   render() {
     return (
       <div>
-          <button onClick={this.incrementCounter}>
+        <input
+          type="text"
+          value={this.state.name}
+          onChange={(e) => {
+            this.setState({ name: e.target.value });
+          }}
+        ></input>
+        <button onClick={this.incrementCounter}>
           you have clicked {this.state.count} times
-          </button>
+        </button>
       </div>
-    )
+    );
   }
 }
 
-export default ClassCounterOne
+export default ClassCounterOne;
--- react-hooks/src/Components/HookCounter1.js
+++ react-hooks/src/Components/HookCounter1.js
@@ -1,17 +1,25 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
 function HookCounter1() {
-    const [count, setcount] = useState(0);
-    useEffect(() => {
-      document.title=`Clicked ${count} times`;
-    })
-    // We are bascically asking react to execute a fn that is passed as an
-    // argument everytime the component runs
+  const [count, setcount] = useState(0);
+  const [name, setName] = useState("");
+  useEffect(() => {
+    console.log("Document title changed");
+    document.title = `Clicked ${count} times`;
+  },[count]); //the array of values the effect depends upon, if the values in this
+//   array changes, then only the effect renders
+  // We are bascically asking react to execute a fn that is passed as an
+  // argument everytime the component runs
   return (
     <div>
-        <button onClick={()=>setcount(count+1)}>Clicked {count} times</button>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={() => setcount(count + 1)}>Clicked {count} times</button>
     </div>
-  )
+  );
 }
 
-export default HookCounter1
+export default HookCounter1;
