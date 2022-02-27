6a9f2d9dfce9aa4f49faa40561db2c4c94ee421b
thesurajkmr <surajkmr32@gmail.com>
2022-02-27 12:36:12 +0530

    hook 21

--- reducer-hook/src/App.js
+++ reducer-hook/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import CounterOne from './Components/CounterOne';
 import CounterTwo from './Components/CounterTwo';
+import CounterThree from './Components/CounterThree';
 
 function App() {
   return (
     <div className="App">
       {/* <CounterOne/> */}
-      <CounterTwo/>
+      {/* <CounterTwo/> */}
+      <CounterThree/>
     </div>
   );
 }
