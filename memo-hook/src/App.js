e471567bca46ceff7dbbe099b394a36031415bb0
thesurajkmr <surajkmr32@gmail.com>
2022-03-01 10:40:26 +0530

    hook 29

--- memo-hook/src/App.js
+++ memo-hook/src/App.js
@@ -2,13 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import Counter from './components/Counter';
 import FocusInput from './components/FocusInput';
+import ClassTimer from './components/ClassTimer';
+import HookTimer from './components/HookTimer';
 
 function App() {
 
   return (
     <div className="App">
       {/* <Counter/> */}
-      <FocusInput/>
+      {/* <FocusInput/> */}
+      <ClassTimer/>
+      <HookTimer/>
     </div>
   );
 }
