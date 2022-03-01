6ee36a3f668d8c919d50c1a11038430699db8b54
thesurajkmr <surajkmr32@gmail.com>
2022-03-01 10:05:09 +0530

    hook 28

--- memo-hook/src/App.js
+++ memo-hook/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './components/Counter';
+import FocusInput from './components/FocusInput';
 
 function App() {
 
   return (
     <div className="App">
-      <Counter/>
+      {/* <Counter/> */}
+      <FocusInput/>
     </div>
   );
 }
