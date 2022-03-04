50b41e05be646c805779ce73e8b684ff1f778638
thesurajkmr <surajkmr32@gmail.com>
2022-03-04 08:18:07 +0530

    tsx 7

--- typescript1/src/App.tsx
+++ typescript1/src/App.tsx
@@ -1,16 +1,11 @@
 import React from "react";
 import "./App.css";
-import { Button } from "./Components/Button";
-import { Input } from "./Components/Input";
-
+import { Container } from "./Components/Container";
 
 function App() {
-  return (
-    <div className="App">
-      {/* <Button handleClick={(event,id)=>console.log("Button Clicked",event,1)}/> */}
-    <Input value={''} handleChange={(event)=>console.log(event)}></Input>
-    </div>
-  );
+  return <div className="App">
+    <Container styles={{border:'1px solid black', padding:'2rem'}}/>
+  </div>
 }
 
 export default App;
---
+++ typescript1/src/Components/Container.tsx
@@ -0,0 +1,10 @@
+type containerProps={
+    styles:React.CSSProperties
+}
+
+export const Container = (props:containerProps) => {
+  return <div style={props.styles}>
+    Text Content goes here
+    </div>
+  
+}
