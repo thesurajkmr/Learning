ae8fed25d468def4acbc64f0d25d51be55f22419
thesurajkmr <surajkmr32@gmail.com>
2022-03-04 11:42:15 +0530

    tsx 13updated

--- typescript1/src/Components/State/Counter.tsx
+++ typescript1/src/Components/State/Counter.tsx
@@ -16,8 +16,13 @@ type updateAction = {
 type resetAction = {
   type: "reset";
 };
+
+type CounterAction=updateAction | resetAction
+// This method right here is called discriminated union in typescript
+// and recommended approach for writting typescript function
+
 const initialstate = { count: 0 };
-function reducer(state: CounterState, action: updateAction | resetAction) {
+function reducer(state: CounterState, action: CounterAction) {
   switch (action.type) {
     case "increment":
       return { count: state.count + action.payload };
